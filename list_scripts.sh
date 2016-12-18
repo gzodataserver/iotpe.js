2ba741e1f685b476b88fa186d1d51b12f9b4fe19
Jonas Colmsjö <jonas.colmsjo@gizur.com>
2016-12-18 11:42:07 +0100

    Added support for remote MySQL host in shell scripts

--- list_scripts.sh
+++ list_scripts.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-if [  $# -ne 2 ]
+if [  $# -ne 3 ]
 then
-  echo -e "\nUsage:\n$0 mysql_username mysql_password\n"
+  echo -e "\nUsage:\n$0 mysql_host mysql_username mysql_password\n"
   exit 1
 fi
 
 cat > /tmp/script.sql <<EOF
-use $1;
+use $2;
 select * from scripts\G
 EOF
 
-mysql -u$1 -p$2 < /tmp/script.sql
+mysql -h$1 -u$2 -p$3 < /tmp/script.sql
--- load_script.sh
+++ load_script.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-if [  $# -ne 4 ]
+if [  $# -ne 5 ]
 then
-  echo -e "\nUsage:\n$0 mysql_username mysql_password script_name file_to_load\n"
+  echo -e "\nUsage:\n$0 mysql_host mysql_username mysql_password script_name file_to_load\n"
   exit 1
 fi
 
 cat > /tmp/script.sql <<EOF
 use $1;
 create table if not exists scripts(name varchar(255), script text);
-delete from scripts where name = '$3';
-insert into scripts(name,script) values ('$3', load_file('$(pwd)/$4'));
+delete from scripts where name = '$4';
+insert into scripts(name,script) values ('$4', load_file('$(pwd)/$5'));
 EOF
 
-# insert into scripts('name','script') values ('$3',  );
-
-mysql -u$1 -p$2 < /tmp/script.sql
+mysql -h$1 -u$2 -p$3 < /tmp/script.sql
